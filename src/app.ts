9746babba88de56514638fa1ec6f5e097db1a71e
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-07-18 08:09:24 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
-import cors from 'cors'
-import express, { Application, NextFunction, Request, Response } from 'express'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/users/user.route'
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/users/user.route';
 
-const app: Application = express()
-// const port = 3000
+const app: Application = express();
+const port = 3000;
 
-app.use(cors())
+app.use(cors());
 // parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Application routes
-app.use('/api/v1/users/', UserRoutes)
+app.use('/api/v1/users/', UserRoutes);
 
 // for testing
 app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  // res.send(`App running on port ${port}`)
+  res.send(`App running on port ${port}`);
   // throw new ApiError(400, 'Wow! Error is Here!!')
-  next(Promise.reject(new Error('Unhandled promise rejected')))
-})
+  next(Promise.reject(new Error('Unhandled promise rejected')));
+});
 
 // global error handler
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-export default app
+export default app;
