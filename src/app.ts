f873156544bc4779286c005f0aae837c7d0179fa
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-07-20 18:15:47 +0600

    code formatted for reuse

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { UserRoutes } from './app/modules/users/user.route';
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route';
+import routes from './app/routes';
 
 const app: Application = express();
 const port = 3000;
@@ -13,8 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Application routes
-app.use('/api/v1/users/', UserRoutes);
-app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
+app.use('/api/v1', routes);
 
 // for testing
 app.get('/', async (req: Request, res: Response, next: NextFunction) => {
