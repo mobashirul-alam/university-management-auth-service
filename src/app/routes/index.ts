66bb68c740d37b1d227eae039549328fd65d8a24
mobashirul-alam <mobashirulalamseam@gmail.com>
2023-07-27 21:31:47 +0600

    academic faculty and department invented

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { UserRoutes } from '../modules/users/user.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 const router = express.Router();
 
 const moduleRoutes = [
@@ -11,6 +13,14 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
+  },
+  {
+    path: '/academic-departments',
+    route: academicDepartmentRoutes,
   },
 ];
 
